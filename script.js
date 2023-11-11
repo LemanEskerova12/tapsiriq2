face66255b0509246a9990f9d6eee799b7dc45ca
LemanEskerova12 <eskerevaleman559@gmail.com>
2023-11-11 14:29:20 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let numberOffilms = +prompt('Nece kinoya baxmisiniz');
+
+const personalMovieDB = {
+    count: numberOffFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false 
+};
+
+personalMovieDB.movies['ad'] = 4
+
+let cavab1 = prompt('Axrinci baxdiginiz film')
+let cavab2 = +prompt('Nece bal ile qiymetlendirirsiniz')
+
+personalMovieDB.movies[cavab1]=cavab2;
+
+console.log(personalMovieDB);
